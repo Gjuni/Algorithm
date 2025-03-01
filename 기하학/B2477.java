df570f5511eed58262df200a196c1bf047b3d081
Gjuni <kmj06178@gmail.com>
2025-03-01 22:59:52 +0900

    B2477 solved 침외밭 문제

--- 기하학/B2477.java
+++ 기하학/B2477.java
@@ -7,6 +7,12 @@
  *            1: 동쪽, 2: 서쪽, 3: 남쪽, 4: 북쪽
  * 
  *  해    설 : 
+ *              width와 height가 존재할 때 
+ *              작은 상자의 크기는 
+ *          
+ *              min_height = 가장 큰 width에서 index + 1, index -1의 길이를 구해서 빼주면 되고
+ *              min_width = 가장 큰 height에서 index +1, index -1의 길이를 구해서 빼주면 됨 
+ *              ** index -1은 (index +5)%6과 동일함 변이 총 6개니 나머지 6으로 나눠 index 범위를 넘지 않게 구할 수 있음
  * 
  *  시간 복잡도 : 
  * 
@@ -28,78 +34,39 @@ public class B2477 {
 
         int fruits = Integer.parseInt(input.readLine());
 
-        int height_arr[] = new int[7];
-        int weight_arr[] = new int[7];
+        int array[] = new int[6];
+        int side_array[] = new int[6];
         
+        int max_height = 0;
         int max_height_index = 0;
+        int max_weight = 0;
         int max_weight_index = 0;
 
         for(int i =0; i< 6; i++) {
             StringTokenizer token = new StringTokenizer(input.readLine());
 
-            int side = Integer.parseInt(token.nextToken());
-            int lenght = Integer.parseInt(token.nextToken());
+            side_array[i] = Integer.parseInt(token.nextToken());
+            array[i] = Integer.parseInt(token.nextToken());
 
-            if(side == 1 || side == 2) {
-                height_arr[i] = lenght;
-                
-                if(max_height_index < height_arr[i]) {
+            if(side_array[i] == 1 || side_array[i] == 2) {                
+                if(max_height < array[i]) {
+                    max_height = array[i];
                     max_height_index = i;
                 }
             } else {
-                weight_arr[i] = lenght;
-
-                if(max_weight_index < weight_arr[i]) {
+                if(max_weight < array[i]) {
+                    max_weight = array[i];
                     max_weight_index = i;
                 }
             }
         }
 
-        // min_wegith 값을 알고 싶으면 max_height 값을 이용
-        // min_height 값을 알고 싶으면 max_weight 값을 이용
+        int min_weight = Math.abs(array[(max_height_index+1)%6] - array[(max_height_index+5)%6]);
+        int min_height = Math.abs(array[(max_weight_index+1)%6] - array[(max_weight_index+5)%6]);
 
-        int min_height = 0;
-        int min_weight = 0;
+        int result = (max_height * max_weight) - (min_height * min_weight);
 
-        int max_height_index_minus = max_height_index -1;
-        int max_height_index_plus = max_height_index + 1;
-
-        if(max_height_index_minus == -1) {
-            max_height_index_minus = 6;
-        }
-
-        if (max_height_index_plus == 7) {
-            max_height_index_plus = 0;
-        }
-
-        int max_weight_index_minus = max_weight_index -1;
-        int max_weight_index_plus = max_weight_index +1;
-
-        if(max_weight_index_minus == -1) {
-            max_weight_index_minus = 6;
-        }
-
-        if(max_weight_index_plus == 7) {
-            max_weight_index_plus = 0;
-        }
-
-        min_height = height_arr[max_weight_index_minus] - height_arr[max_weight_index_plus];
-        min_weight = weight_arr[max_height_index_minus] - weight_arr[max_height_index_plus];
-
-        if(min_height < 0) {
-            min_height *= -1;
-        }
-
-        if(min_weight < 0) {
-            min_weight *= -1;
-        }
-
-        output.write(String.valueOf(height_arr[max_height_index]) + " "+ String.valueOf(weight_arr[max_weight_index])+"\n");
-        output.write(String.valueOf(min_height) + " " + String.valueOf(min_weight)+"\n");
-
-        int total_wide = height_arr[max_height_index] * weight_arr[max_weight_index] - min_height * min_weight;
-
-        output.write(String.valueOf(total_wide * fruits));
+        output.write(String.valueOf(result*fruits));
 
         input.close();
         output.close();
