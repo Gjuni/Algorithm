1aa97e998c6fb894effd740f59a589edadf59965
Gjuni <kmj06178@gmail.com>
2025-05-26 14:20:34 +0900

    B1316 solved 그룹 단어 체커 문제

--- 구현/B1316.java
+++ 구현/B1316.java
@@ -1,16 +1,22 @@
 /**
- *  문제 이름 : 분수 찾기
+ *  문제 이름 : 그룹 단어 체커
  * 
  *  난 이  도 : 실버 5
  * 
- *  아이디어 : N!에서 0의 갯수를 구하는 프로그램을 작성
+ *  아이디어 : 각 문자가 연속해서 나타나는 경우만 그룹 단어라고 지치한다.
+ *              cczbcd는 모두 연속해서 나타난 문자열이지만
+ *              cbcccbc는 연속해서 나타나지 않았기 때문에 그룹 단어가 아니다.
  * 
- *  해    설 : 처음에는 x0 의 갯수나 x00의 갯수를 세보려고 했으나
- *              10이나 100은 2*5로 만들어짐, 단 2는 5보다 많을 수 밖에 없기에 5의 갯수를 세면 되지 않을까 생각함
- *          -> 틀림
- * 
- *          해결책 : 
+ *  해    설 : 
+ *              단순 indexing문제이다.
  *              
+ *              현재 위치의 알파뱃과 다음 위치의 알파뱃을 본 뒤 
+ *              Class에 정의해놓은 Count값이 0이라면 그냥 그 값을 증가시킨다.
+ * 
+ *              curr_alpha와 next_alpha가 다르다면 
+ *                  next_alpha를 확인 후 그 값이 0이라면 curr_alpha Count값을 증가시키고
+ *             
+ *                  next_alpha의 값이 0 이상이라면 이미 전에 나왔다는 뜻이기 때문에 return시킨다.
  * 
  *  시간 복잡도 : 
  * 
@@ -24,18 +30,79 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+
+class Count {
+    char alphabet;
+    int count;
+
+    Count(char alphabet, int count) {
+        this.alphabet = alphabet;
+        this.count = count;
+    }
+}
+
 public class B1316 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int num = Integer.parseInt(input.readLine());
-        int answer = 0;
+    static Count alpha[] = new Count[26];
+    static int answer = 0;
 
-        for(int i = 1; i <= num; i++) {
-            if(i%5 == 0) {
-                answer++;
+    public static void solve(String word) {
+        if (word.length() == 1) {
+            answer++;
+            return;
+        }
+
+
+        for (int i = 0; i < word.length()-1; i++) {
+            int curr_alpha_index = word.charAt(i) - 'a';
+            int next_alpga_index = word.charAt(i+1)- 'a';
+
+
+            if(alpha[curr_alpha_index].alphabet == alpha[next_alpga_index].alphabet) {
+                alpha[curr_alpha_index].count++;
+            } else if (alpha[curr_alpha_index].alphabet != alpha[next_alpga_index].alphabet) {
+                
+                if(alpha[next_alpga_index].count == 0) {
+                    alpha[curr_alpha_index].count++;
+                } else if (alpha[next_alpga_index].count > 0) {
+                    return;
+                }
             }
+        }
+
+        int curr_alpha_index = word.charAt(word.length()-2) - 'a';
+        int next_alpga_index = word.charAt(word.length()-1) - 'a';
+
+
+            if(alpha[curr_alpha_index].alphabet == alpha[next_alpga_index].alphabet) {
+                alpha[next_alpga_index].count++;
+            } else if (alpha[curr_alpha_index].alphabet != alpha[next_alpga_index].alphabet) {
+                
+                if(alpha[next_alpga_index].count == 0) {
+                    alpha[next_alpga_index].count++;
+                } else if (alpha[next_alpga_index].count > 0) {
+                    return;
+                }
+            }
+
+        answer++;
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int tc = Integer.parseInt(input.readLine());
+
+        for (int g = 0; g < tc; g++) {
+
+            for (int i = 0; i < 26; i++) {            
+                alpha[i] = new Count((char)('a'+i), 0);
+            }
+
+            String word = input.readLine();
+
+            solve(word);
         }
 
         output.write(String.valueOf(answer));
